04e78912b638e009f381d2315f8d568b9314ef0e
victortonneberg <84105735+victortoneberg@users.noreply.github.com>
2025-03-08 22:55:14 +0100

    Byttet fra useParams til prop
    Oppdaget i ettertid at oppgaven sier man må sende med category som prop.
    
    Rettet opp, nå skal det oppfylle kravet.

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,14 @@ function App() {
   return (
         <Layout>
             <Routes>
-                <Route path="/:category" element={<Resources />} />
+                <Route path='/html' element={<Resources category="html" />} />
+                <Route path='/css' element={<Resources category="css" />} />
+                <Route path='/javascript' element={<Resources category="javascript" />} />
+                <Route path='/react' element={<Resources category="react" />} />
+                <Route path='/headless-cms' element={<Resources category="headless-cms" />} />
             </Routes>
         </Layout>
-)
-
+    )
 }
 
 export default App
