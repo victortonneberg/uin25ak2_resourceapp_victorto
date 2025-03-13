369353066fce4b188c2cfb64d1a50f321e897dc8
victortonneberg <84105735+victortoneberg@users.noreply.github.com>
2025-03-13 10:07:33 +0100

    Småfix

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -14,8 +14,7 @@ export default function Resources({category}) {
                     {/*Bruker .map for å iterere gjennom de filtrerte ressursene.*/}
                     {resourcesFilter.map(resource => (
                         <li key={resource.title}> {/*Bruker resource.title som en unik key. Dette gjør at jeg slipper å bruke index. En "fallgruve" jeg ser her er at det kan skape problemer hvis man får 2 like titles i ressurser.js. */}
-                            <h3>{resource.title}</h3> {/*Viser navnet på ressursen som er hentet.*/}
-                            <a href={resource.url} target="_blank">{resource.url}</a> {/*Link som hentes fra resources.*/}
+                            <a href={resource.url} target="_blank">{resource.title}</a> {/*Link og title som hentes fra resources.*/}
                         </li>
                     ))}
                 </ul>
